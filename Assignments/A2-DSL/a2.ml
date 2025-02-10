342b9a5dd7d10e743ea5a8c4bb8482bccb226cbb
Yash-Rawat-IIT-D <dbringery@LAPTOP-JEVMCPHD.>
2025-02-10 09:30:15 +0000

    Error Raise

--- Assignments/A2-DSL/a2.ml
+++ Assignments/A2-DSL/a2.ml
@@ -67,13 +67,13 @@ let rec create (n: int) (x: float) : vector =
 let rec dim (vec_float : vector) : int = 
   match vec_float with
   | [] -> raise DimensionError  
-  | x::[] -> 1
-  | x::xs -> 1 + dim xs
+  | _::[] -> 1
+  | _::xs -> 1 + dim xs
 ;; 
-let rec is_legal_dim (vec_float : vector) : bool  = 
+let is_legal_dim (vec_float : vector) : bool  = 
   match vec_float with
   | [] -> false             
-  | x::xs -> true
+  | _::_ -> true
 ;;
 let rec is_zero (v : vector) : bool = 
   if(not (is_legal_dim v)) then
@@ -92,7 +92,7 @@ let rec is_zero_close (v : vector) : bool =
     | [] -> raise DimensionError 
     | x :: [] -> if (f_eq_z x) then true else false
     | x::xs -> if (f_eq_z x) then is_zero_close xs else false
-let rec unit (n : int) (j : int) : vector = 
+let unit (n : int) (j : int) : vector = 
   if(n < 1) then 
     raise DimensionError
   else if (j > n || j < 1) then 
@@ -264,7 +264,7 @@ let rec type_of (e : expr) : types =
     (
       match (type_of e1) with
       | Scalar -> Scalar
-      | Vector n -> Scalar
+      | Vector _ -> Scalar
       | _ -> raise (Wrong(e))
     )
 
@@ -280,7 +280,7 @@ let rec type_of (e : expr) : types =
       match type_of e1 with
       | Bool -> Bool
       | Scalar -> Bool
-      | Vector n -> Bool     
+      | Vector _ -> Bool     
     )
 
   | Cond (e1, e2, e3) -> 
@@ -308,7 +308,7 @@ let rec type_of (e : expr) : types =
 
 let rec eval (e : expr) : values = 
 
-  let my_static_type_check = type_of e in
+  let _ = type_of e in
 
   match e with
   | T -> B true
@@ -320,14 +320,24 @@ let rec eval (e : expr) : values =
       match (eval e1, eval e2) with
       | B b1, B b2 -> B (b1 || b2)
       | S s1, S s2 -> S (s1 +. s2)
-      | V v1, V v2 -> V (addv v1 v2)
+      | V v1, V v2 -> 
+        (
+          try
+            V (addv v1 v2)
+          with _ -> raise (Wrong(e))
+        )
       | _ -> raise (Wrong(e))
     )
   | Sub (e1, e2) ->
     (
       match (eval e1, eval e2) with
       | S s1, S s2 -> S (s1 -. s2)
-      | V v1, V v2 -> V (addv v1 (inv v2))
+      | V v1, V v2 -> 
+        (
+          try  
+            V (addv v1 (inv v2))
+          with _ -> raise (Wrong(e))
+        )
       | _ -> raise (Wrong(e))
     )
   | Inv e1 ->
@@ -335,41 +345,79 @@ let rec eval (e : expr) : values =
       match eval e1 with
       | B b -> B (not b)
       | S s -> S (-1.0 *. s)
-      | V v -> V (inv v)
+      | V v -> 
+        (
+          try
+            V (inv v)
+          with _ -> raise (Wrong(e))
+        )
     )
   | ScalProd (e1, e2) -> 
     (
       match (eval e1, eval e2) with
       | B b1, B b2 -> B (b1 && b2)
       | S s1, S s2 -> S (s1 *. s2)
-      | S s, V v -> V (scale s v)
-      | V v, S s -> V (scale s v)
+      | S s, V v -> 
+        (
+          try
+            V (scale s v)
+          with _ -> raise (Wrong(e))
+        )
+      | V v, S s -> 
+        (
+          try
+            V (scale s v)
+          with _ -> raise (Wrong(e))
+        )
       | _ -> raise (Wrong(e))
     )
   | DotProd (e1, e2) -> 
     (
       match (eval e1, eval e2) with
-      | V v1, V v2 -> S (dot_prod v1 v2)
+      | V v1, V v2 -> 
+        (
+          try
+            let dot_prod_val = dot_prod v1 v2 in
+            S dot_prod_val
+          with _ -> raise (Wrong(e))
+        )
       | _ -> raise (Wrong(e))
     )
   | Mag e1 -> 
     (
       match eval e1 with
       | S s -> S (abs_float s)
-      | V v -> S (length v)
+      | V v -> 
+        ( try 
+            let v_mag = length v in
+            S v_mag
+          with _ -> raise (Wrong(e))
+        )
       | _ -> raise (Wrong(e))
     )
   | Angle (e1, e2) -> 
     (
       match (eval e1, eval e2) with
-      | V v1, V v2 -> S (angle v1 v2)
+      | V v1, V v2 -> 
+        (
+          try
+            let theta_v1_v2 = angle v1 v2 in   
+            S theta_v1_v2
+          with _ -> raise (Wrong(e))
+        )
       | _ -> raise (Wrong(e))
     )
   | IsZero e1 -> 
     (
       match eval e1 with
       | S s -> B (f_eq_z s)
-      | V v -> B (is_zero_close v)
+      | V v -> 
+        (
+          try
+            let bval = is_zero v in
+            B bval
+          with _ -> raise (Wrong(e))
+        )
       | _ -> raise (Wrong(e))
     )
   | Cond (e1, e2, e3) -> 
@@ -381,131 +429,8 @@ let rec eval (e : expr) : values =
 ;;
 
 
+(*===================================================================================*)
 
 (*===================================================================================*)
 
-let print_result (desc : string) (result : bool) : unit =
-  Printf.printf "%s: %s\n" desc (if result then "Passed" else "Failed")
-;;
-
-(* Test cases for type_of function *)
-
-let test_type_bool () = 
-  let tests = [
-    (T, Bool, "Type of T");
-    (F, Bool, "Type of F");
-    (Add (T, F), Bool, "Type of Add (T, F)");
-    (Inv (T), Bool, "Type of Inv T");
-    (ScalProd(Inv (T),Add(Inv (T),Inv (F))), Bool, "Type of ScalProd(Inv (T),Add(Inv (T),Inv (F)))");
-    (ScalProd (T, F), Bool, "Type of ScalProd (T, F)");
-    (IsZero (ConstV [0.0; 0.0; 0.0; 0.0]), Bool, "Type of IsZero (ConstV [0.0; 0.0; 0.0; 0.0])");
-    ] in
-  List.iter (fun (expr, expected_type, desc) ->
-    let result = try type_of expr = expected_type with _ -> false in
-    print_result desc result
-  ) tests
-;;
-
-let test_type_scalar() = 
-  let tests = [
-    (ConstS 3.1415, Scalar, "Type of ConstS 3.14");
-    (Add (ConstS 192.01, ConstS 57.44), Scalar, "Type of Add (ConstS 192.01, ConstS 57.44)");
-    (Sub (Add (ConstS 192.01, ConstS 57.44), Add (ConstS 46.44, ConstS 341.76)), Scalar, "Type of Sub (Add (ConstS 192.01, ConstS 57.44), Add (ConstS 46.44, ConstS 341.76)) :");
-    (Inv ( DotProd (ConstV [1.0; 2.0; 3.0], ConstV [4.0; 5.0; 6.0])), Scalar, "Type of Inv ( DotProd (ConstV [1.0; 2.0; 3.0], ConstV [4.0; 5.0; 6.0])) : ");
-    (Mag (ScalProd (ConstS 2.0, ConstV [1.0; 2.0])), Vector 2, "Type of ScalProd (ConstS 2.0, ConstV [1.0; 2.0])");
-    (DotProd (ScalProd (ConstS 5.43, ConstV [1.33; 2.23]), ScalProd (ConstV [7.76; 2.35],ConstS 1.11)), Scalar, "DotProd (ScalProd (ConstS 5.43, ConstV [1.33; 2.23]), ScalProd (ConstV [7.76; 2.35],ConstS 1.11))) : ");
-    (* (Mag (ConstV [3.0; 4.0]), Scalar, "Type of Mag (ConstV [3.0; 4.0])"); *)
-    (Angle (ConstV [1.0; 2.0; 3.0], ConstV [-3.0; -4.0; 5.0]), Scalar, "Type ofAngle (ConstV [1.0; 2.0; 3.0], ConstV [-3.0; -4.0; 5.0]) : ");
-    (IsZero (ConstS 0.0), Bool, "Type of IsZero (ConstS 0.0)");
-    (Cond (T, ConstS 1.0, ConstS 2.0), Scalar, "Type of Cond (T, ConstS 1.0, ConstS 2.0)")
-  ] in
-  List.iter (fun (expr, expected_type, desc) ->
-    let result = try type_of expr = expected_type with _ -> false in
-    print_result desc result
-  ) tests
-
-  let test_type_vector () = 
-    let tests = [
-      (ConstV [1.0; 2.0; 3.0], Vector 3, "Type of ConstV [1.0; 2.0; 3.0]");
-      (Add (ConstV [1.0; 2.0], ConstV [3.0; 4.0]), Vector 2, "Type of Add (ConstV [1.0; 2.0], ConstV [3.0; 4.0])");
-      (Sub (ConstV [1.0; 2.0], ConstV [3.0; 4.0]), Vector 2, "Type of Sub (ConstV [1.0; 2.0], ConstV [3.0; 4.0])");
-      (Inv (ConstV [1.0; 2.0]), Vector 2, "Type of Inv (ConstV [1.0; 2.0])");
-      (ScalProd (ConstS 2.0, ConstV [1.0; 2.0]), Vector 2, "Type of ScalProd (ConstS 2.0, ConstV [1.0; 2.0])");
-      (DotProd (ConstV [1.0; 2.0], ConstV [3.0; 4.0]), Scalar, "Type of DotProd (ConstV [1.0; 2.0], ConstV [3.0; 4.0])");
-      (Mag (ConstV [3.0; 4.0]), Scalar, "Type of Mag (ConstV [3.0; 4.0])");
-      (Angle (ConstV [1.0; 0.0], ConstV [0.0; 1.0]), Scalar, "Type of Angle (ConstV [1.0; 0.0], ConstV [0.0; 1.0])");
-      (IsZero (ConstV [0.0; 0.0]), Bool, "Type of IsZero (ConstV [0.0; 0.0])");
-      (Cond (T, ConstV [1.0; 2.0], ConstV [3.0; 4.0]), Vector 2, "Type of Cond (T, ConstV [1.0; 2.0], ConstV [3.0; 4.0])")
-    ] in
-    List.iter (fun (expr, expected_type, desc) ->
-      let result = try type_of expr = expected_type with _ -> false in
-      print_result desc result
-    ) tests
-  ;;
-
-let test_type () =
-
-  let tests = [
-    (T, Bool, "Type of T");
-    (F, Bool, "Type of F");
-    (ConstS 3.14, Scalar, "Type of ConstS 3.14");
-    (ConstV [1.0; 2.0; 3.0], Vector 3, "Type of ConstV [1.0; 2.0; 3.0]");
-    (Add (ConstS 1.0, ConstS 2.0), Scalar, "Type of Add (ConstS 1.0, ConstS 2.0)");
-    (Sub (ConstS 1.0, ConstS 2.0), Scalar, "Type of Sub (ConstS 1.0, ConstS 2.0)");
-    (Inv (ConstS 1.0), Scalar, "Type of Inv (ConstS 1.0)");
-    (ScalProd (ConstS 2.0, ConstV [1.0; 2.0]), Vector 2, "Type of ScalProd (ConstS 2.0, ConstV [1.0; 2.0])");
-    (DotProd (ConstV [1.0; 2.0], ConstV [3.0; 4.0]), Scalar, "Type of DotProd (ConstV [1.0; 2.0], ConstV [3.0; 4.0])");
-    (Mag (ConstV [3.0; 4.0]), Scalar, "Type of Mag (ConstV [3.0; 4.0])");
-    (Angle (ConstV [1.0; 0.0], ConstV [0.0; 1.0]), Scalar, "Type of Angle (ConstV [1.0; 0.0], ConstV [0.0; 1.0])");
-    (IsZero (ConstS 0.0), Bool, "Type of IsZero (ConstS 0.0)");
-    (Cond (T, ConstS 1.0, ConstS 2.0), Scalar, "Type of Cond (T, ConstS 1.0, ConstS 2.0)")
-  ] in
-  List.iter (fun (expr, expected_type, desc) ->
-    let result = try type_of expr = expected_type with _ -> false in
-    print_result desc result
-  ) tests
-;;
-
-(* Test cases for eval function *)
-let test_eval () =
-    let tests = [
-    (T, B true, "Eval T");
-    (F, B false, "Eval F");
-    (ConstS 3.14, S 3.14, "Eval ConstS 3.14");
-    (ConstV [1.0; 2.0; 3.0], V [1.0; 2.0; 3.0], "Eval ConstV [1.0; 2.0; 3.0]");
-    (Add (ConstS 1.0, ConstS 2.0), S 3.0, "Eval Add (ConstS 1.0, ConstS 2.0)");
-    (Sub (ConstS 1.0, ConstS 2.0), S (-1.0), "Eval Sub (ConstS 1.0, ConstS 2.0)");
-    (Inv (ConstS 1.0), S (-1.0), "Eval Inv (ConstS 1.0)");
-    (ScalProd (ConstS 2.0, ConstV [1.0; 2.0]), V [2.0; 4.0], "Eval ScalProd (ConstS 2.0, ConstV [1.0; 2.0])");
-    (DotProd (ConstV [1.0; 2.0], ConstV [3.0; 4.0]), S 11.0, "Eval DotProd (ConstV [1.0; 2.0], ConstV [3.0; 4.0])");
-    (Mag (ConstV [3.0; 4.0]), S 5.0, "Eval Mag (ConstV [3.0; 4.0])");
-    (Angle (ConstV [1.0; 0.0], ConstV [0.0; 1.0]), S (Float.pi /. 2.0), "Eval Angle (ConstV [1.0; 0.0], ConstV [0.0; 1.0])");
-    (IsZero (ConstS 0.0), B true, "Eval IsZero (ConstS 0.0)");
-    (Cond (T, ConstS 1.0, ConstS 2.0), S 1.0, "Eval Cond (T, ConstS 1.0, ConstS 2.0)")
-  ] in
-  List.iter (fun (expr, expected_value, desc) ->
-    let result = try eval expr = expected_value with _ -> false in
-    print_result desc result
-  ) tests
-;;
-
-(* Run all tests *)
-let () =
-  print_endline "===================================================================================";
-  print_endline "Running type_bool tests:";
-  test_type_bool();
-  print_endline "===================================================================================";
-  print_endline "Running type_scalar tests:";
-  test_type_scalar();
-  print_endline "===================================================================================";
-  print_endline "Running eval tests:";
-  test_type_vector();
-  
-  print_endline "===================================================================================";
-  print_endline "Running eval tests:";
-  test_eval ();
-;;
-
-
-(*===================================================================================*)
 
--- Assignments/A2-DSL/testcases.ml
+++ Assignments/A2-DSL/testcases.ml
@@ -1,4 +1,28 @@
 open A2
 
+(* Test Cases of type boolean *)
+
 let%test "type_of T" = (type_of T = Bool)
 let%test "type_of F" = (type_of F = Bool)
+let%test "type_of (IsZero (ScalProd (ConstS 2.45,ConstV (addv([5.23; -67.8; -324.34; -12.341], [3.58; 96.9; 8.037; 0.546])))))" 
+= (type_of (IsZero (ScalProd (ConstS 2.45, ConstV (addv([5.23; -67.8; -324.34; -12.341], [3.58; 96.9; 8.037; 0.546]))))) = Bool)
+let%test "type_of Add(Add(ScalProd(T,F),ScalProd(F,T)),Add(ScalProd(T,T),ScalProd(F,F)))" 
+= (type_of (Add(Add(ScalProd(T,F),ScalProd(F,T)),Add(ScalProd(T,T),ScalProd(F,F)))) = Bool)
+let%test "type_of type_of (Inv (ScalProd (T,Add(F,(ScalProd (T,Add(F,T))))))) = Bool"
+= (type_of (Inv (ScalProd (T,Add(F,(ScalProd (T,Add(F,T))))))) = Bool)
+
+(* Test Cases of type scalar *)
+
+let%test "type_of (ConstS 3.1415)" = (type_of (ConstS 3.14) = Scalar)
+let%test "type_of (Add (ConstS 127.550, ConstS 1289.134))" = (type_of (Add (ConstS 127.550, ConstS 1289.134)) = Scalar)
+let%test "type_of (ScalProd (Add (ConstS 192.01, ConstS 57.44), ScalProd (ConstS 4.44, ConstS 321.76)))" = (type_of (Sub (Add (ConstS 192.01, ConstS 57.44), Add (ConstS 4.44, ConstS 321.76)) = Scalar))
+let%test "type_of (Inv ( DotProd (ConstV [1.111; 2.222; 3.333], ConstV [4.444; 5.555; 6.666]))" = (type_of (Inv ( DotProd (ConstV [1.111; 2.222; 3.333], ConstV [4.444; 5.555; 6.666])) = Scalar))
+let%test "type_of (Mag (ScalProd (ConstS -1.56, ConstV [26.47; 8.457; 8.23; 4.7; 3.24]))" = (type_of (Mag (ScalProd (ConstS -1.56, ConstV [26.47; 8.457; 8.23; 4.7; 3.24])) = Scalar))
+let%test "type_of (DotProd (ScalProd (ConstS 5.43, ConstV [1.33; 2.23]), ScalProd (ConstV [7.76; 2.35],ConstS 1.11))" = (type_of (DotProd (ScalProd (ConstS 5.43, ConstV [1.33; 2.23]), ScalProd (ConstV [7.76; 2.35],ConstS 1.11)) = Scalar))
+
+(* Test Cases of type vector *)
+
+let%test "type_of (ConstV [1.0; 2.0; 3.0])" = (type_of (ConstV [1.0; 2.0; 3.0]) = Vector 3)
+let%test "type_of (Add (ConstV [2.983; 457.07; 8.93; 0.245], ConstV [2.93; 6.78; 4.501; 2.655]))" = (type_of (Add (ConstV [2.983; 457.07; 8.93; 0.245], ConstV [2.93; 6.78; 4.501; 2.655]) = Vector 4))
+let%test "type_of (Cond (IsZero (ConstS 0.0), (ConstV [1.0; 2.0; 3.0]), (ScalProd (ConstV,ConstS 5.66))))" = (type_of (Cond (IsZero (ConstS 0.0), ConstV [1.0; 2.0], ConstV [3.0; 4.0]) = Vector 2))
+let%test "type_of (Inv (ScalProd (Add(ConstV [1.618; 2.718; 3.14]), ConstV [8.18; 2.331; 92.22])), ConstS 5.22))" = ((type_of (Inv (ScalProd (Add(ConstV [1.618; 2.718; 3.14]), ConstV [8.18; 2.331; 92.22])), ConstS 5.22)) = Vector 3)
